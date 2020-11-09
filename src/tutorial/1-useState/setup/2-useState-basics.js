d6cce681f9f18478bae7ef5adf2cabcfa53233b6
lu1151 <luxi.li@outlook.com>
2020-11-08 22:56:09 -0500

    11/08Commit

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("hello"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value,handler);
+
+  const [apple, setApple] = useState("random title")
+
+  const handleClick = () => {
+    if (apple == "random title") {
+      setApple("hello");
+    } else {
+      setApple("random title")
+    }
+  };
+
+  return (
+    <>
+      <h1>{apple}</h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </>
+  )
 };
 
 export default UseStateBasics;
