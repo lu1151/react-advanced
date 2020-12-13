32f0d73bd416080bdcb5e8ec9f99ecdbdd6547e9
lu1151 <luxi.li@outlook.com>
2020-12-12 22:20:13 -0500

    12/12Commit

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,53 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("No values");
+    }
+  }
+
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-cintrol">
+            <label htmlFor="firstName">Name: </label>
+            <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+          </div>
+          <div className="form-cintrol">
+            <label htmlFor="email">Email: </label>
+            <input type="text" id="email" name="email" value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+        {
+          people.map((person) => {
+            const { id, firstName, email } = person;
+            return <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          })
+        }
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
