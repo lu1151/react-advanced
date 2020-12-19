6a3afc35c8ea3fe494a550a6e2df94d8e85f5882
lu1151 <luxi.li@outlook.com>
2020-12-18 23:51:48 -0500

    12/18Commit

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,9 +2,70 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
-
+const reducer = (state, action) => {
+  console.log(state);
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload]
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "please enter value"
+    };
+  };
+  throw new Error("no matching action type");
+};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: ""
+}
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  // const [people, setPeople] = useState(data);
+  // const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem })
+      setName("");
+      // setShowModal(true);
+      // setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      // setName("");
+    }
+    else {
+      dispatch({ type: "NO_VALUE" });
+      // setShowModal(true)
+    }
+  }
+
+  return (
+    <>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <button type='submit'>add </button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
