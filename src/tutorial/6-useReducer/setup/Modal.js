39af294330570812ad26b5c823148103346abd0b
lu1151 <luxi.li@outlook.com>
2020-12-19 22:41:27 -0500

    12/19

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,9 +1,17 @@
 import React, { useEffect } from 'react';
 
-const Modal = ({ modalContent }) => {
-  return <div className="modal">
-    <p>{modalContent}</p>
-  </div>;
+const Modal = ({ modalContent, closeModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  });
+
+  return (
+    <div className="modal">
+      <p>{modalContent}</p>
+    </div>
+  );
 };
 
 export default Modal;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,27 +1,8 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-// reducer function
-const reducer = (state, action) => {
-  console.log(state);
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload]
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item added",
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return {
-      ...state,
-      isModalOpen: true,
-      modalContent: "please enter value"
-    };
-  };
-  throw new Error("no matching action type");
-};
+import { reducer } from "./reducer";
+
 const defaultState = {
   people: [],
   isModalOpen: false,
@@ -48,9 +29,13 @@ const Index = () => {
     }
   }
 
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  }
+
   return (
     <>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
@@ -59,8 +44,11 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button onClick={() =>
+              dispatch({ type: "REMOVE_ITEM", payload: person.id })
+            }>remove</button>
           </div>
         );
       })}
